6e2d5a74a7121b5d13210aebc95c6319c09d70e6
Guillermo Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-07-22 16:04:57 -0300

    add user stocks register

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,9 @@
 class Stock < ApplicationRecord
-  
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
 
     client = IEX::Api::Client.new(
@@ -7,7 +11,16 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    client.price('AAPL')
+    begin 
+      new(ticker:ticker_symbol, name:client.company(ticker_symbol).company_name, 
+        last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end  
+    #client.price('AAPL')
   end
 
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 end
